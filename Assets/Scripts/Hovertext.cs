dd0c92072a127ef7d8ae252e8d4b82d3fccaeb8e
unknown <anna.mueler-2@mni.thm.de>
2021-02-25 17:09:29 +0100

    new Rail popup Text

---
+++ Assets/Scripts/Hovertext.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hovertext : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if (OverRail.textstatus == "off")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
